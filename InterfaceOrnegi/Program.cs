acd026495a4180f2d69d7214f6fc36e1b73df2e8
salihkomut <slhkomut@gmail.com>
2024-01-09 21:48:03 +0300

    bitti

--- InterfaceOrnegi/Program.cs
+++ InterfaceOrnegi/Program.cs
@@ -6,11 +6,50 @@ using System.Threading.Tasks;
 
 namespace InterfaceOrnegi
 {
+    interface Kutuphane
+    {
+        void Oku(string adi);
+    }
+    class Kitap : Kutuphane
+    {
+        public void Oku(string kitapAi)
+        {
+            Console.WriteLine("şu kitap okundu : "+kitapAi);
+            
+        }
+    }
+    class Ansiklopedi : Kutuphane
+    {
+        public void Oku(string adi)
+        {
+            Console.WriteLine("ansiklopedi okundu : "+adi);
+        }
+    }
+    class Dergi : Kutuphane
+    {
+        public void Oku(string adi)
+        { Console.WriteLine("dergi okundu : "+adi); }
+    }
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Merhaba dünya");
+
+            Kutuphane okunacak;
+            
+            
+            okunacak = new Kitap();
+            okunacak.Oku("Dede Korkut");
+            
+
+            okunacak = new Ansiklopedi();
+            okunacak.Oku("cumhuriyet ansiklopedisi");
+
+            okunacak = new Dergi();
+            okunacak.Oku("monstar");
+
+            Console.ReadKey();
+
         }
     }
 }
